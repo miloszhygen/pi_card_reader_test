6ced7184c2de97deb64dca3d28149f7d83f5177d
Tommy Østgaard <tommy.ostgaard@gmail.com>
2019-03-07 14:42:05 +0200

    cardReader

---
+++ cardReader.js
@@ -0,0 +1,46 @@
+"use strict";
+const _ = require("underscore");
+const mfrc522 = require("./node_modules/mfrc522-rpi/index.js");
+
+const cardArray = ['a44e81e', 'd80d673'];
+let COUNT = 0;
+
+//# Init WiringPi with SPI Channel 0
+mfrc522.initWiringPi(0);
+let interval = null;
+
+const addToCount = _.debounce(function (callback) {
+  callback();
+}, 1000, true);
+
+module.exports = {
+  start: function (callback) {
+    console.log('Listening for cards...');
+    interval = setInterval(function(){
+        //# reset card
+        mfrc522.reset();
+        let response = mfrc522.findCard();
+
+        //# Get the UID of the card
+        response = mfrc522.getUid();
+        if (!response.status) {
+            console.log("UID Scan Error");
+            return;
+        }
+
+        const uid = response.data;
+        const uidValue = uid[0].toString(16) + uid[1].toString(16) + uid[2].toString(16) + uid[3].toString(16);
+
+        if (cardArray.indexOf(uidValue) >= 0) {
+          addToCount(function () {
+            callback(uidValue);
+          })
+        }
+        //# Stop
+        mfrc522.stopCrypto();
+    }, 500);
+  },
+  stop: function () {
+    clearInterval(interval);
+  }
+}
