c4992d606c93db643ded2ce4177da61444c9cc6a
Tommy Østgaard <tommy.ostgaard@gmail.com>
2019-03-07 15:01:46 +0200

    Added dev environment

--- cardReader.js
+++ cardReader.js
@@ -1,45 +1,59 @@
 "use strict";
-const _ = require("underscore");
-const mfrc522 = require("./node_modules/mfrc522-rpi/index.js");
-
-const cardArray = ['a44e81e', 'd80d673'];
-let COUNT = 0;
-
-//# Init WiringPi with SPI Channel 0
-mfrc522.initWiringPi(0);
 let interval = null;
+const cardArray = ['a44e81e', 'd80d673'];
 
-const addToCount = _.debounce(function (callback) {
-  callback();
-}, 1000, true);
+if (process.env.NODE_ENV === 'production') {
+  const _ = require("underscore");
+  const mfrc522 = require("./node_modules/mfrc522-rpi/index.js");
 
-module.exports = {
-  start: function (callback) {
-    console.log('Listening for cards...');
-    interval = setInterval(function(){
-        //# reset card
-        mfrc522.reset();
-        let response = mfrc522.findCard();
+  //# Init WiringPi with SPI Channel 0
+  mfrc522.initWiringPi(0);
 
-        //# Get the UID of the card
-        response = mfrc522.getUid();
-        if (!response.status) {
-            return;
-        }
+  const addToCount = _.debounce(function (callback) {
+    callback();
+  }, 1000, true);
 
-        const uid = response.data;
-        const uidValue = uid[0].toString(16) + uid[1].toString(16) + uid[2].toString(16) + uid[3].toString(16);
+  module.exports = {
+    start: function (callback) {
+      console.log('Listening for cards...');
+      interval = setInterval(function(){
+          //# reset card
+          mfrc522.reset();
+          let response = mfrc522.findCard();
 
-        if (cardArray.indexOf(uidValue) >= 0) {
-          addToCount(function () {
-            callback(uidValue);
-          })
-        }
-        //# Stop
-        mfrc522.stopCrypto();
-    }, 500);
-  },
-  stop: function () {
-    clearInterval(interval);
+          //# Get the UID of the card
+          response = mfrc522.getUid();
+          if (!response.status) {
+              return;
+          }
+
+          const uid = response.data;
+          const uidValue = uid[0].toString(16) + uid[1].toString(16) + uid[2].toString(16) + uid[3].toString(16);
+
+          if (cardArray.indexOf(uidValue) >= 0) {
+            addToCount(function () {
+              callback(uidValue);
+            })
+          }
+          //# Stop
+          mfrc522.stopCrypto();
+      }, 500);
+    },
+    stop: function () {
+      clearInterval(interval);
+    }
+  }
+} else {
+  module.exports = {
+    start: function (callback) {
+      console.log('Running fake card readings ...');
+      interval = setInterval(function() {
+        const randomCardArrayIndex = Math.floor(Math.random()*cardArray.length);
+          callback(cardArray[randomCardArrayIndex]);
+      }, 2000);
+    },
+    stop: function () {
+      clearInterval(interval);
+    }
   }
 }
