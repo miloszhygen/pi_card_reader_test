3a6977c92bf849ba97239947d43a4a6fff68c4f4
milosz <miloszhygen@gmail.com>
2019-03-07 15:06:49 +0200

    Added firebase stuff

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 "use strict";
 const cardReader = require('./cardReader');
 
+const firebase = require("firebase");
+const firebaseConfig = require('./firebaseConfig');
+
+console.log(firebaseConfig);
+
+firebase.initializeApp(firebaseConfig);
+
 cardReader.start(function(uidValue) {
   console.log(uidValue);
 });
