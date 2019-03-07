a869eb55cc76e8a8ed7952125e6c44bdd7b890a0
milosz <miloszhygen@gmail.com>
2019-03-07 15:17:43 +0200

    more firebase

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ console.log(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 
+
+firebase.database().ref('users/').once('value').then(function(snapshot) {
+  // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+  console.log(snapshot.val());
+  return snapshot.val()
+});
+
+
 cardReader.start(function(uidValue) {
   console.log(uidValue);
 });
