4fb04da231fcbe98dc5ffeb4f3c091f2ec82e989
Tommy Østgaard <tommy.ostgaard@gmail.com>
2019-03-07 16:18:21 +0200

    sound

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 "use strict";
 const cardReader = require('./cardReader');
+const player = require('play-sound')({})
 
 const firebase = require("firebase");
 const firebaseConfig = require('./firebaseConfig');
-
-console.log(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 
@@ -19,28 +18,17 @@ firebase.database().ref('users/').once('value').then(function(snapshot) {
 cardReader.start(function(uidValue) {
   console.log(uidValue);
 
-
-
-  // function toggleStar(postRef, uid) {
+  player.play('BleepBlop.aif', function(err) {
+    if (err) throw err
+  })
     firebase.database().ref('users/'+uidValue).transaction(function(post) {
      if (post) {
        if (!post.count) {
          post.count = 1;
-        //  post.stars[uid] = null;
        } else {
          post.count++;
-        //  if (!post.stars) {
-        //    post.stars = {};
-        //  }
-        //  post.stars[uid] = true;
        }
      }
      return post;
    });
-  // }
-
-
-
-
-
 });
