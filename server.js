5ba3b86d355a1efb3ae85936cf593e3be07ecf22
Tommy Østgaard <tommy.ostgaard@gmail.com>
2019-03-08 10:02:33 +0200

    sound

--- server.js
+++ server.js
@@ -1,43 +1,32 @@
 "use strict";
 const cardReader = require('./cardReader');
-// let Sound = function() {};
-// let music = null;
-// if (process.env.NODE_ENV === 'production') {
-    const Sound = require('node-aplay');
-// }
-
+const Sound = require('node-aplay');
 const firebase = require("firebase");
 const firebaseConfig = require('./firebaseConfig');
 
 firebase.initializeApp(firebaseConfig);
 
-
 firebase.database().ref('users/').once('value').then(function(snapshot) {
-  // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
   console.log('Connected to firebase');
 });
 
-// if (process.env.NODE_ENV === 'production') {
-  console.log('Set music');
-  let music = new Sound('./BleepBlop.wav');
+let music = new Sound('./YappShort.wav');
 
-  music.on('complete', function () {
-    console.log('Sound has finished playing!');
-  });
-// }
+music.on('complete', function () {
+  console.log('Sound has finished playing!');
+});
 
 cardReader.start(function(uidValue) {
   console.log(uidValue);
-    console.log('Playing music');
-    music.play();
-    firebase.database().ref('users/'+uidValue).transaction(function(post) {
-     if (post) {
-       if (!post.count) {
-         post.count = 1;
-       } else {
-         post.count++;
-       }
+  music.play();
+  firebase.database().ref('users/'+uidValue).transaction(function(post) {
+   if (post) {
+     if (!post.count) {
+       post.count = 1;
+     } else {
+       post.count++;
      }
-     return post;
-   });
+   }
+   return post;
+  });
 });
